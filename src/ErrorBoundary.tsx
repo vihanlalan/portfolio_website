9a3d430996553b33f427e5df23c77b91d810cf49
Vihan Gunjan Lalan <vihan.lalan01@svkmmumbai.onmicrosoft.com>
2026-06-24 18:20:32 +0530

    Fix TypeScript errors in ErrorBoundary

--- src/ErrorBoundary.tsx
+++ src/ErrorBoundary.tsx
@@ -1,18 +1,28 @@
-import React from "react";
-export class ErrorBoundary extends React.Component {
-  constructor(props) {
+import React, { ReactNode } from "react";
+
+interface Props {
+  children?: ReactNode;
+}
+
+interface State {
+  hasError: boolean;
+  error: Error | null;
+}
+
+export class ErrorBoundary extends React.Component<Props, State> {
+  constructor(props: Props) {
     super(props);
     this.state = { hasError: false, error: null };
   }
-  static getDerivedStateFromError(error) {
+  static getDerivedStateFromError(error: Error): State {
     return { hasError: true, error };
   }
   render() {
     if (this.state.hasError) {
       return (
         <div style={{background: "white", color: "red", padding: "20px", position: "absolute", zIndex: 9999, top: 0, left: 0}}>
-          {this.state.error.toString()}<br/>
-          {this.state.error.stack}
+          {this.state.error?.toString()}<br/>
+          {this.state.error?.stack}
         </div>
       );
     }
